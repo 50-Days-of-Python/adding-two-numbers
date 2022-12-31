ca6daae5293ac336950fdfd9655b5afd62c287b3
Likith666 <97726177+Likith666@users.noreply.github.com>
2022-12-31 13:59:33 +0530

    Create test_1.py

---
+++ test_1.py
@@ -0,0 +1,12 @@
+import unittest
+import main
+
+# DO NOT TOUCH THE BELOW CODE
+
+class Test_1(unittest.TestCase):
+  def test_01(self):
+    self.assertEqual(sum_0f_num(5,4), 9)
+
+  def test_02(self):
+    self.assertEqual(sum_0f_num(6,94), 100)
+    
