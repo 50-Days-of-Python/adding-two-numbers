b41317a938cc4e7375048706e54671ffedde473f
Likith666 <97726177+Likith666@users.noreply.github.com>
2022-12-31 14:36:48 +0530

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -5,10 +5,10 @@ import main
 
 class Test_1(unittest.TestCase):
   def test_01(self):
-    self.assertEqual(main.sum_0f_num(5,4), 9)
+    self.assertEqual(main.highest_of(5,4), 5)
 
   def test_02(self):
-    self.assertEqual(main.sum_0f_num(6,94), 100)
+    self.assertEqual(main.highest_of(6,94), 94)
     
 if __name__ == '__main__':
   unittest.main(verbosity=2)
